fc50b9a4b0af80e1af9aec79270f1b1b219d22bc
Amrish-Krgtech <amrish.r@krgtech.com>
2025-05-30 16:00:42 +0530

    First commit

---
+++ src/themes/theme.module.ts
@@ -0,0 +1,96 @@
+import { NgModule } from '@angular/core';
+import { AccordionModule } from 'primeng/accordion';
+import { AnimateModule } from 'primeng/animate';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { InputTextModule } from 'primeng/inputtext';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
+import { RippleModule } from 'primeng/ripple';
+import { StyleClassModule } from 'primeng/styleclass';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { VirtualScrollerModule } from 'primeng/virtualscroller';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { BadgeModule } from 'primeng/badge';
+import { MenuModule } from 'primeng/menu';
+import { AvatarModule } from 'primeng/avatar';
+import { SidebarModule } from 'primeng/sidebar';
+import { PanelModule } from 'primeng/panel';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { DividerModule } from 'primeng/divider';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+import { TreeSelectModule } from 'primeng/treeselect';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PasswordModule } from 'primeng/password';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { StepperModule } from 'primeng/stepper';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FieldsetModule } from 'primeng/fieldset';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { ImageModule } from 'primeng/image';
+import { CalendarModule } from 'primeng/calendar';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+
+@NgModule({
+  declarations: [],
+  exports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AnimateOnScrollModule,
+    AccordionModule,
+    ButtonModule,
+    VirtualScrollerModule,
+    InputTextModule,
+    MessageModule,
+    MessagesModule,
+    ToastModule,
+    ToggleButtonModule,
+    AnimateModule,
+    CardModule,
+    RippleModule,
+    StyleClassModule,
+    IconFieldModule,
+    InputIconModule,
+    AutoFocusModule,
+    PanelMenuModule,
+    BadgeModule,
+    MenuModule,
+    AvatarModule,
+    SidebarModule,
+    PanelModule,
+    ScrollPanelModule,
+    DividerModule,
+    BreadcrumbModule,
+    ConfirmPopupModule,
+    TableModule,
+    DialogModule,
+    TreeSelectModule,
+    PasswordModule,
+    InputSwitchModule,
+    StepperModule,
+    InputNumberModule,
+    ProgressSpinnerModule,
+    InputTextareaModule,
+    FieldsetModule,
+    MultiSelectModule,
+    ImageModule,
+    CalendarModule,
+    SelectButtonModule,
+    FloatLabelModule,
+    TieredMenuModule,
+  ],
+})
+export class ThemeModule {}
